3ae1a58aa053c70b0e9fd7e1d3f9cc7e2cbca642
gumpen <shuhei1993727@gmail.com>
2020-09-17 17:28:46 +0900

    fix getter

--- lib/saml_idp/signature_builder.rb
+++ lib/saml_idp/signature_builder.rb
@@ -6,7 +6,7 @@ module SamlIdp
 
     def initialize(signed_info_builder, cert=nil)
       self.signed_info_builder = signed_info_builder
-      self.x509_certificate = x509_certificate(cert)
+      @x509_certificate = cert
     end
 
     def raw
@@ -22,8 +22,8 @@ module SamlIdp
       end
     end
 
-    def x509_certificate(cert)
-      (cert || SamlIdp.config.x509_certificate)
+    def x509_certificate
+      (@x509_certificate || SamlIdp.config.x509_certificate)
       .to_s
       .gsub(/-----BEGIN CERTIFICATE-----/,"")
       .gsub(/-----END CERTIFICATE-----/,"")
